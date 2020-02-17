d5b1720d22a21dd346eb82327c16612141f4c91c
Matt Payne <Payne@MattPayne.org>
2020-02-16 22:27:29 -0600

    From https://medium.com/@admin_86118/testing-typescript-node-with-jest-6bf5db18119c

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,14 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageThreshold: {
+      global: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80
+       }
+   },
+   coverageReporters: ['json', 'lcov', 'text', 'clover']
 }
+
